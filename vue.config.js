26eb1a7d575ddbfc133bd34348c8df50c51d2fd5
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2021-09-30 13:17:55 +0300

    devServer check? (

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    disableHostCheck: true
+  }
   // publicPath: process.env.NODE_ENV === 'production' ? '/dka/' : '/'
 }
