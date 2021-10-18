fd5492e115a157e0c83a585e7cac150e93130019
Murat Mukhtarov <murat@1100.email>
2021-10-18 11:33:56 +1100

    Add logger, fix root directory of express server

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const path = require('path')
-
+const winston = require('winston')
+const expressWinston = require('express-winston')
 // optional: allow environment to specify port
 const port = process.env.PORT || 8080
 
 // create server instance
 const app = express()
-// bind the request to an absolute path or relative to the CWD
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.json()
+  ),
+  meta: false,
+  msg: 'HTTP {{req.method}} {{req.url}}',
+  expressFormat: true,
+  colorize: false,
+  ignoreRoute: function (req, res) { return false }
+}))
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname, '/dist/index.html')
+  const options = {
+    root: path.join(__dirname, 'dist'),
+    dotfiles: 'deny'
+  }
+
+  res.sendFile('index.html', options, function (err) {
+    if (err) {
+      res.status(err.status).end()
+    }
+  })
 })
+
+app.use(express.static(path.join(__dirname, 'dist')))
 
 // start the server
 app.listen(port, () => console.log(`Listening on port ${port}`))
