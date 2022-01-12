7c401f50251c06cad86db915694bbd19b0a3ed11
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2022-01-13 00:39:51 +0200

    worker rebuild

--- worker/webpack.config.worker.js
+++ worker/webpack.config.worker.js
@@ -1,20 +1,6 @@
 // const path = require('path')
 
 module.exports = {
-  resolve: {
-    fallback: {
-      fs: false,
-      tls: false,
-      net: false,
-      path: false,
-      zlib: false,
-      http: false,
-      https: false,
-      stream: false,
-      crypto: false,
-      'crypto-browserify': require.resolve('crypto-browserify')
-    }
-  },
   context: require('path').resolve(__dirname),
   entry: ['@babel/polyfill', './src/index.js'],
   output: {
